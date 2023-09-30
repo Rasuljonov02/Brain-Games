fc190b8933cedefcb42c6ec01b7aa14e9a8afb2c
Muhammad <muhammadjonrasuljonov2002@gmail.com>
2023-09-30 13:09:47 +0500

    6

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import "./main.css";
 const contener: HTMLDivElement = document.querySelector("#contener");
 const body: HTMLBodyElement = document.querySelector("body");
+const taimlar: HTMLParagraphElement = document.querySelector(".taimlar");
+const son: HTMLParagraphElement = document.querySelector(".son");
 let a: string[];
 
 cellchiz();
@@ -17,16 +19,22 @@ function cellchiz() {
 		contener.appendChild(cell);
 	}
 }
+
 let o: number = 0;
+let sanoq: number = 0;
 function boyaydi() {
 	const cellar: NodeListOf<HTMLParagraphElement> = document.querySelectorAll(".cellar");
 
 	cellar.forEach((cell: HTMLParagraphElement, index: number) => {
 		cell.innerText = a[index] || "";
 		cell.addEventListener("click", () => {
+			// hato
 			if (cell.innerText === "") {
 				cell.classList.add("gameover");
 				cell.classList.add("winner");
+				sanoq = 0;
+				son.innerText = `${sanoq}`;
+
 				body.style.pointerEvents = "none";
 				setTimeout(() => {
 					cellchiz();
@@ -34,15 +42,19 @@ function boyaydi() {
 					body.style.pointerEvents = "all";
 				}, 2000);
 			} else {
+				// togri
 				o++;
-
 				if (o === 6) {
 					setTimeout(() => {
 						cellchiz();
 						boyaydi();
-				
+						body.style.pointerEvents = "all";
 					}, 2000);
-					o=0;
+					body.style.pointerEvents = "none";
+
+					sanoq++;
+					o = 0;
+					son.innerText = `${sanoq}`;
 				}
 				cell.classList.add("active");
 			}
@@ -54,4 +66,26 @@ function boyaydi() {
 			cell.classList.remove("active");
 		});
 	}, 2000);
+
+	updateTimer();
+}
+
+let s: number = 30;
+let m: number = 0;
+
+function updateTimer() {
+	// if (s < 0) {
+	// 	s = 0;
+	// 	m--;
+	// }
+	s--;
+	taimlar.innerText = `${m < 10 ? "0" + m : m} : ${s < 10 ? "0" + s : s}`;
+
+	if (s === 0) {
+		cellchiz();
+		boyaydi();
+
+	} else {
+		// setTimeout(updateTimer, 1000);
+	}
 }
