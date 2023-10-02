7c289fea7abe99cbaf41e3ed88cb4d71f182be18
Muhammad <muhammadjonrasuljonov2002@gmail.com>
2023-10-02 17:10:35 +0500

    chala

--- src/data.ts
+++
@@ -1,2 +0,0 @@
-export const NUMBER_OF_CELLS = 9;
-export const NUMBER_OF_INIT_CELLS = 3;
--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,54 @@
 import "./main.css";
-import { NUMBER_OF_CELLS, NUMBER_OF_INIT_CELLS } from "./data";
+
 const son: HTMLParagraphElement = document.querySelector(".son")!;
 const boardElm: HTMLDivElement = document.querySelector("#board")!;
 let cells: string[] = [];
 let correctAnswers = 0;
-let a: number = 0;
-let aa: number = 0;
-let winner1: number = 0;
-let winner2: number = 0;
-let b: number = 3;
+let a: number = 0; // bosilgan belgilar
+let aa: number = 0; // hozilgi level
+let winner1: number = 3; // belgilar
+let winner2: number = 9; // kataklar
+let b: number = 4; // grid
+let col: number = 3;
+let row: number = 3;
+
 // HANDLE FUNCTIONS
 function handleCell(cell: HTMLDivElement, cellIdx: number) {
 	if (!cells[cellIdx]) {
-		renderCells();
 		console.log(5);
+
 		aa = 0;
 		a = 0;
 		son.innerText = `${aa}`;
-
 		winner1 = 0;
 		winner2 = 0;
-		b = 3;
-		return setTimeout(init, 1000);
+		b = 0;
+		init();
 	}
-
 	correctAnswers++;
 	cell.classList.add("active");
 
-	if (correctAnswers === NUMBER_OF_INIT_CELLS + winner1) {
+	if (correctAnswers === winner1) {
 		// togri ga rekshiradi
 
+		console.log(6);
 		setTimeout(init, 1000);
 	}
 
 	a++;
-	if (a === NUMBER_OF_INIT_CELLS + winner1) {
+	// togri bosilganda
+	if (a === winner1) {
+		// togriga rekshiradi va chizadi
+
 		aa++;
 		winner1++;
 		a = 0;
-		son.innerText = `${aa}`;
-		console.log(aa);
+		son.innerText = `Level ${aa}`;
+
 		if (winner1 % 2 == 0) {
-			winner2 += 4;
+			col++;
+			row++;
+			winner2 = col * row;
 			boardElm.style.gridTemplateColumns = `repeat(${b}, 1fr)`;
 			boardElm.style.gridTemplateRows = `repeat(${b}, 1fr)`;
 
@@ -61,6 +68,7 @@ function renderCells() {
 		cellElm.classList.add("cell");
 
 		if (cell) {
+			winner1;
 			cellElm.classList.add("active");
 			cellElm.innerText = cell;
 			initCellElms.push(cellElm);
@@ -76,13 +84,10 @@ function renderCells() {
 }
 
 // LOGIC FUNCTIONS
-
 function init() {
 	correctAnswers = 0;
-	cells = new Array(NUMBER_OF_INIT_CELLS + winner1).fill("👍");
-	const stayCells = new Array(NUMBER_OF_CELLS + winner2 - (NUMBER_OF_INIT_CELLS + winner1)).fill(
-		""
-	);
+	cells = new Array(winner1).fill("👍");
+	const stayCells = new Array(winner2 - winner1).fill("");
 
 	cells = [...cells, ...stayCells].sort(() => Math.random() - 0.5);
 
