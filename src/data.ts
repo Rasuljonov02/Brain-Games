c38c12844e4e560b4013592c55e94b612d04d32e
Muhammad <muhammadjonrasuljonov2002@gmail.com>
2023-10-02 20:30:36 +0500

    65

---
+++ src/data.ts
@@ -0,0 +1,2 @@
+export const NUMBER_OF_CELLS = 3; // cell
+export const NUMBER_OF_INIT_CELLS = 3; // belgi
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
 import "./main.css";
-
+import { NUMBER_OF_CELLS, NUMBER_OF_INIT_CELLS } from "./data";
 const son: HTMLParagraphElement = document.querySelector(".son")!;
 const boardElm: HTMLDivElement = document.querySelector("#board")!;
+const soat: HTMLParagraphElement = document.querySelector(".soat")!;
 let cells: string[] = [];
+
 let correctAnswers = 0;
-let a: number = 0; // bosilgan belgilar
-let aa: number = 0; // hozilgi level
-let winner1: number = 3; // belgilar
-let winner2: number = 9; // kataklar
-let b: number = 4; // grid
+let a: number = 0;
+let aa: number = 0;
+let winner1: number = 0;
+let winner2: number = 9;
+let b: number = 4;
 let col: number = 3;
 let row: number = 3;
 
@@ -16,41 +18,42 @@ let row: number = 3;
 function handleCell(cell: HTMLDivElement, cellIdx: number) {
 	if (!cells[cellIdx]) {
 		console.log(5);
-
 		aa = 0;
 		a = 0;
-		son.innerText = `${aa}`;
+		son.innerText = ` Level${aa}`;
 		winner1 = 0;
 		winner2 = 0;
+		winner2 = col * row;
 		b = 0;
-		init();
+		return setTimeout(init, 1000);
 	}
+
 	correctAnswers++;
 	cell.classList.add("active");
 
-	if (correctAnswers === winner1) {
+	if (correctAnswers === NUMBER_OF_INIT_CELLS + winner1) {
 		// togri ga rekshiradi
 
-		console.log(6);
 		setTimeout(init, 1000);
 	}
 
 	a++;
-	// togri bosilganda
-	if (a === winner1) {
+
+	if (a === NUMBER_OF_INIT_CELLS + winner1) {
 		// togriga rekshiradi va chizadi
 
 		aa++;
 		winner1++;
 		a = 0;
 		son.innerText = `Level ${aa}`;
-
+		console.log(aa);
 		if (winner1 % 2 == 0) {
 			col++;
 			row++;
 			winner2 = col * row;
 			boardElm.style.gridTemplateColumns = `repeat(${b}, 1fr)`;
 			boardElm.style.gridTemplateRows = `repeat(${b}, 1fr)`;
+			console.log(winner2);
 
 			b++;
 		}
@@ -84,14 +87,27 @@ function renderCells() {
 }
 
 // LOGIC FUNCTIONS
+
 function init() {
 	correctAnswers = 0;
-	cells = new Array(winner1).fill("👍");
-	const stayCells = new Array(winner2 - winner1).fill("");
+	cells = new Array(NUMBER_OF_INIT_CELLS + winner1).fill("👍");
+	const stayCells = new Array(winner2 - (NUMBER_OF_INIT_CELLS + winner1)).fill(""); // shu qatorda hato
+	console.log(winner2);
 
 	cells = [...cells, ...stayCells].sort(() => Math.random() - 0.5);
 
 	renderCells();
 }
+let s: number = 29;
+let sekund = setInterval(() => {
+	if (s === 0) {
+		setTimeout(init, 0);
+		s = 29;
+		// clearInterval(sekund);
+	}
+	soat.innerText = `Taim 00:${s < 10 ? "0" + s : s}`;
+
+	s--;
+}, 1000);
 
 init();
